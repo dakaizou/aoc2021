efc5fe132f290a053127bf0ea220cd34b4029fb9
Dakai Tzou <dakaizou@gmail.com>
2022-12-14 17:00:06 -0500

    add day1

---
+++ src/d1/mod.rs
@@ -0,0 +1,45 @@
+use crate::util::read_lines;
+use crate::util::Windowing;
+
+#[allow(dead_code)]
+fn p1() {
+    let mut lines = read_lines("input/d1");
+    let mut prev = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
+    let mut inc_count = 0;
+
+    for line in lines {
+        let cur = line.unwrap().parse::<i32>().unwrap();
+        if cur > prev {
+            inc_count += 1;
+        }
+        prev = cur;
+    }
+
+    println!("{inc_count}");
+}
+
+#[allow(dead_code)]
+fn p2() {
+    let mut lines = read_lines("input/d1").window(3);
+    let mut prev = lines
+        .next()
+        .unwrap()
+        .iter()
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum::<i32>();
+    let mut inc_count = 0;
+
+    for window in lines {
+        let cur = window.iter().map(|s| s.parse::<i32>().unwrap()).sum();
+        if cur > prev {
+            inc_count += 1;
+        }
+        prev = cur;
+    }
+
+    println!("{inc_count}");
+}
+
+pub fn main() {
+    p2();
+}
---
+++ src/util.rs
@@ -0,0 +1,80 @@
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+use std::iter::Map;
+
+pub struct Window<T, I>
+where
+    T: Clone,
+    I: Iterator<Item = T>,
+{
+    it: I,
+    cur: VecDeque<T>,
+    size: usize,
+}
+
+impl<T, I> Window<T, I>
+where
+    T: Clone,
+    I: Iterator<Item = T>,
+{
+    fn owned_current_window(&self) -> Vec<T> {
+        let mut res = Vec::new();
+        for i in self.cur.iter() {
+            res.push(i.clone());
+        }
+        res
+    }
+}
+
+impl<T, I> Iterator for Window<T, I>
+where
+    T: Clone,
+    I: Iterator<Item = T>,
+{
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.cur.len() < self.size {
+            self.cur.push_back(self.it.next()?);
+            if self.cur.len() == self.size {
+                return Some(self.owned_current_window());
+            }
+        }
+        self.cur.push_back(self.it.next()?);
+        self.cur.pop_front();
+        Some(self.owned_current_window())
+    }
+}
+
+pub trait Windowing {
+    type Item: Clone;
+    type Iter: Iterator<Item = Self::Item>;
+
+    fn window(self, size: usize) -> Window<Self::Item, Self::Iter>;
+}
+
+impl Windowing for Lines<io::BufReader<File>> {
+    type Item = String;
+
+    type Iter = impl Iterator<Item = String>;
+    // type Iter = Map<Lines<BufReader<File>>, impl FnOnce(Result<String, io::Error>) -> String>;
+
+    fn window(self, size: usize) -> Window<Self::Item, Self::Iter> {
+        let w = Window {
+            it: self.into_iter().map(|r| r.unwrap()),
+            cur: VecDeque::new(),
+            size,
+        };
+        w
+    }
+}
+
+pub fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).expect("file cannot be opened");
+    io::BufReader::new(file).lines()
+}
